7c25cb879c04ac50d75f36587536f530520171dd
Jayesh Betala <jayesh.betala7@gmail.com>
2022-07-17 16:41:03 +0100

    Added Player Sprinting

--- RPG/Assets/Scripts/PlayerMovement.cs
+++ RPG/Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,29 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent agent;
+    private Animator animator;
     private Ray ray;
     private RaycastHit hit;
 
+    private float x;
+    private float z;
+    private float velocitySpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //calculate velocity speed
+        x = agent.velocity.x;
+        z = agent.velocity.z;
+        velocitySpeed = x + z;
+
         if(Input.GetMouseButtonDown(0))
         {
             //changes 2D position of the mouse to effective 3D posiiton for the character
@@ -28,6 +38,15 @@ public class PlayerMovement : MonoBehaviour
             {
                 agent.destination = hit.point;
             }
+        }
+
+        if(velocitySpeed != 0)
+        {
+            animator.SetBool("sprinting", true);
+        }
+        if (velocitySpeed == 0)
+        {
+            animator.SetBool("sprinting", false);
         }
     }
 }
