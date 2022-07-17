40bf6bdb8e441117aed0adce6376c0a0c9d9f468
Jayesh Betala <jayesh.betala7@gmail.com>
2022-07-17 22:23:48 +0100

    Player Character Selection Menu completed after Feedbacks

---
+++ RPG/Assets/Scripts/PlayerStartPosition.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPosition : MonoBehaviour
+{
+    public GameObject[] characters;
+    public Transform spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(characters[SaveScript.pCharacter], spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
